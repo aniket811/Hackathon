ff5a05d580f01d64935b5810bee86dc924b32edb
Aniket Joshi <ajosh4347@gmail.com>
2023-09-11 07:39:09 +0530

    landing site completion and authentication partially initialized

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingsiteComponent } from './landingsite/landingsite.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LandingsiteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingsiteComponent } from './landingsite/landingsite.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingsiteComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
